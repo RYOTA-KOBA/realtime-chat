900d35e6c85e666ba6d13f9fc60d4c42c23260d0
RYOTA <kobaryo04ysh@icloud.com>
2021-07-07 08:05:05 +0900

    feat: 任意のユーザー名を設定可能に

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,56 @@ import { db, timestamp } from "./firebase";
 
 const App: React.FC = () => {
   const [chatdata, setChatdata] = useState([]);
-  // const [isMessageCreated, setIsMessageCreated] = useState(false);
+  const [isMessageCreated, setIsMessageCreated] = useState(false);
   const [formValue, setFormValue] = useState("");
+  const [usernameValue, setUsernameValue] = useState("");
+  const [username, setUsername] = useState("");
+
+  const sendUsername = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setUsername(usernameValue);
+    setUsernameValue("");
+  };
 
   const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    await db.collection("chats").add({
-      message: formValue,
-      created_at: timestamp,
-    });
+    if (username) {
+      await db.collection("chats").add({
+        message: formValue,
+        created_at: timestamp,
+        username: username,
+      });
+    }
     setFormValue("");
   };
 
   useEffect(() => {
     let chats: any = [];
-    db.collection("chats").onSnapshot((snapshots) => {
-      snapshots.docChanges().forEach((change) => {
-        const data = change.doc.data({ serverTimestamps: "estimate" });
-        const changeType = change.type;
+    db.collection("chats")
+      .orderBy("created_at")
+      .onSnapshot((snapshots) => {
+        snapshots.docChanges().forEach((change) => {
+          const data = change.doc.data({ serverTimestamps: "estimate" });
+          const changeType = change.type;
 
-        switch (changeType) {
-          case "added":
-            chats.push({ ...data, id: change.doc.id });
-            break;
-          case "removed":
-            chats = chats.filter((chat: any) => chat.id !== change.doc.id);
-            break;
-          default:
-            break;
-        }
+          switch (changeType) {
+            case "added":
+              chats.push({ ...data, id: change.doc.id });
+              setIsMessageCreated(true);
+              break;
+            case "removed":
+              chats = chats.filter((chat: any) => chat.id !== change.doc.id);
+              break;
+            default:
+              break;
+          }
+        });
+        setChatdata(chats);
+        setIsMessageCreated(false);
       });
-      setChatdata(chats);
-    });
-  }, [setChatdata]);
+  }, [setChatdata, setIsMessageCreated]);
 
   return (
     <>
@@ -45,22 +61,38 @@ const App: React.FC = () => {
       {chatdata &&
         chatdata.map((d: any) => (
           <ul key={d.id}>
-            <li>{d.message}</li>
+            <li>
+              <span>{d.username}</span>
+              &nbsp;
+              {d.message}
+            </li>
           </ul>
         ))}
+      {username !== "" ? (
+        <form onSubmit={sendMessage}>
+          <input
+            type="text"
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+            placeholder="メッセージを入力"
+          />
 
-      <form onSubmit={sendMessage}>
-        <input
-          type="text"
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="メッセージを入力"
-        />
-
-        <button type="submit" disabled={!formValue}>
-          🕊️
-        </button>
-      </form>
+          <button type="submit" disabled={!formValue}>
+            送信
+          </button>
+        </form>
+      ) : (
+        <form onSubmit={sendUsername}>
+          <input
+            type="text"
+            onChange={(e) => setUsernameValue(e.target.value)}
+            placeholder="名前を入力"
+          />
+          <button type="submit" disabled={!usernameValue}>
+            ログイン
+          </button>
+        </form>
+      )}
     </>
   );
 };
