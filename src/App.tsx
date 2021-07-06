74ed65ceaaa0d813209ec3c98949a0c7940e967c
RYOTA <kobaryo04ysh@icloud.com>
2021-07-06 22:22:33 +0900

    初期設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { db } from "./firebase";
 
-function App() {
+const App: React.FC = () => {
+  const [userdata, setUserdata] = useState([]);
+
+  useEffect(() => {
+    db.collection("users")
+      .get()
+      .then((snapshots: any) => {
+        const userArray: any = [];
+        snapshots.docs.forEach((doc: any) => {
+          const data = doc.data();
+          userArray.push({
+            id: doc.id,
+            username: data.username,
+            created_at: data.created_at,
+          });
+        });
+        setUserdata(userArray);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {console.log(userdata)}
+      {userdata &&
+        userdata.map((d: any) => (
+          <ul key={d.id}>
+            <li>{d.username}</li>
+          </ul>
+        ))}
+    </>
   );
-}
+};
 
 export default App;
