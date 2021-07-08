7f7e5246bfc4788c332a75fb3882638762998219
RYOTA <kobaryo04ysh@icloud.com>
2021-07-08 16:41:00 +0900

    feat: add username actionを追加

--- src/reducks/chats/actions.js
+++ src/reducks/chats/actions.js
@@ -6,7 +6,22 @@ export const addMessageAction = (chatState) => {
     payload: {
       id: chatState.id,
       username: chatState.username,
+      message: chatState.message,
       created_at: chatState.created_at,
+    },
+  };
+};
+
+export const ADD_USERNAME = "ADD_USERNAME";
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const addUsernameAction = (chatState) => {
+  return {
+    type: "ADD_USERNAME",
+    payload: {
+      id: "",
+      username: chatState.username,
+      message: "",
+      created_at: "",
     },
   };
 };
