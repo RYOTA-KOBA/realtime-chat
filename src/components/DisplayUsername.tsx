a50471bb00cc160bc8910385443fb1ddfebb46e1
RYOTA <kobaryo04ysh@icloud.com>
2021-07-09 17:32:06 +0900

    fix: DisplayUsernameをcomponentsディレクトリに移動

--- src/components/DisplayUsername.tsx
+++ src/components/DisplayUsername.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getUsername } from "./reducks/chats/selectors";
+import { getUsername } from "../reducks/chats/selectors";
+import { InitialChatsState } from "../reducks/chats/types";
 
 const DisplayUsername: React.FC = () => {
-  const selector = useSelector((state) => state);
+  const selector = useSelector((state: InitialChatsState) => state);
   const username = getUsername(selector);
 
   return <div>{username}</div>;
