38a1e065472207e1a0081b05028aa5cd0e652178
RYOTA <kobaryo04ysh@icloud.com>
2021-07-09 17:31:36 +0900

    fix: selectorの引数に型を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./styles/App.css";
 import { useSelector } from "react-redux";
 import { getUsername } from "./reducks/chats/selectors";
+import { InitialChatsState } from "./reducks/chats/types";
 
 import UsernameForm from "./components/UsernameForm";
 import MessageForm from "./components/MessageForm";
 import ChatList from "./components/ChatList";
-import DisplayUsername from "./DisplayUsername";
+import DisplayUsername from "./components/DisplayUsername";
 
 const App: React.FC = () => {
-  const selector = useSelector((state) => state);
+  const selector = useSelector((state: InitialChatsState) => state);
   const username = getUsername(selector);
 
   return (
