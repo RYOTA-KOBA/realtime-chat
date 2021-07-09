31c5285974131963b5ed6413e8a28c7db05d86d7
RYOTA <kobaryo04ysh@icloud.com>
2021-07-09 21:34:49 +0900

    feat: DisplayTime componentを追加し時間を表示

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
-import { fetchChatsdata } from "../reducks/chats/selectors";
+import { fetchChatsdata, getCreatedAt } from "../reducks/chats/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchChats } from "../reducks/chats/operations";
 import { InitialChatsState } from "../reducks/chats/types";
+import DisplayTime from "./DisplayTime";
 
 type Chats = Partial<{
   id: string;
@@ -27,6 +28,7 @@ const ChatList: React.FC = () => {
           chatsdata.map((chat: Chats) => (
             <li key={chat.id}>
               {chat.username}: {chat.message}
+              <DisplayTime created_at={chat.created_at} />
             </li>
           ))}
       </ul>
